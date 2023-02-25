5816cf712c7fe5a227791300f78df9a51573f445
ashok kumar <spider.akm@gmail.com>
2023-02-25 21:25:59 +0530

    added routes features

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const adminRoutes = require('./routes/admin.js')
+const shopRoutes = require('./routes/shop.js')
 const app = express();
 
 //middlewares
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="number" name="count"><button type="submit">Add Product</button></form>');
-  });
-  
-app.post('/product',(req,res,next) => {
-    console.log(req.body);
-    res.redirect('/');
+app.use('/admin',adminRoutes);
+app.use(shopRoutes)
+
+//404 
+app.use((req,res,next) => {
+    res.status(404).send('<h1> 404 not found </h2>')
 })
-
-
-app.use('/',(req,res,next) => {
-    res.send("<h1> hello ashok   </h1>")
-})
-
 
 
 app.listen(4000)
