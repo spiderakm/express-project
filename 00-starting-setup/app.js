40079d96ef431102105f6c01f6eeccd05d84b37a
ashok kumar <spider.akm@gmail.com>
2023-02-26 21:50:54 +0530

    error with controller task for udemy instructor

---
+++ 00-starting-setup/app.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const controllerError = require('./controller/error')
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(controllerError)
+app.listen(3000);
