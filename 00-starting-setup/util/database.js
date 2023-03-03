2318bbcbd32e1773736ccb5fab259a52a2429405
ashok kumar <spider.akm@gmail.com>
2023-03-03 10:57:04 +0530

    final

--- 00-starting-setup/util/database.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize')
-
-const sequelize = new Sequelize('nodestart','root','nodecomplete',{
-    dialect:'mysql',
-    host: 'localhost'
-})
-
-
-module.exports = sequelize;
